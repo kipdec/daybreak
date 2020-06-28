5e2ed98131514e48b9c4277e90dc1dbe7b1f0353
Kip DeCastro <kipdec@Kips-MacBook-Pro.local>
2020-06-28 06:06:38 -0600

    Added all the menus

--- src/src/index.js
+++ src/src/index.js
@@ -4,6 +4,9 @@ import Phaser from 'phaser';
 import TestDungeon from './levels/TestDungeon';
 import Title from './levels/Title.js';
 import Controls from './levels/Controls.js';
+import Credits from './levels/Credits.js';
+import About from './levels/About.js';
+import GameOver from './levels/GameOver.js';
 
 document.body.style.cursor = 'none';  // removes cursor so we can use crosshair instead
 
@@ -22,7 +25,7 @@ var config = {
       debug: true
     }
   },
-  scene: [Title,TestDungeon,Controls],
+  scene: [Title,TestDungeon,Controls,Credits,About,GameOver],
 };
 
 export var game = new Phaser.Game(config);
--- src/src/levels/About.js
+++ src/src/levels/About.js
@@ -1,48 +1,26 @@
-// TestDungeon.js
+// About.js
 
 import Phaser, { GameObjects } from 'phaser'
 import { game } from '../index'
-import title from '../assets/tilemaps/introscreen.json';
-import pcImg from '../assets/talia.png';
-import titleScreenSheet from '../assets/spritesheets/title_screen.png';
-import fireball from '../assets/fireball.png';
-import reticleImg from '../assets/reticle.png';
-import Attack from '../common/Attack.js';
-import enemyImg from '../assets/pc.png'
-import bearImg from '../assets/bear.png';
-import heartImg from '../assets/heart.png';
+import about from '../assets/tilemaps/about.json';
+import aboutScreenSheet from '../assets/spritesheets/about_screen.png';
 
-export default class Title extends Phaser.Scene {
+export default class About extends Phaser.Scene {
   constructor() {
-    super({ key: 'Title' })
+    super({ key: 'About' })
   }
 
   preload () {
-    this.load.image('title_tiles', titleScreenSheet);
-    this.load.tilemapTiledJSON('title_map', title);
-    this.load.spritesheet('pc',pcImg,{
-      frameWidth: 20,
-      frameHeight: 20
-    });
-    this.load.image('attack', fireball);
-    this.load.image('enemy', enemyImg);
-    this.load.spritesheet('bear', bearImg, {
-      frameWidth: 14,
-      frameHeight: 15
-    });
-    this.load.image('reticle', reticleImg);
-    this.load.spritesheet('heart', heartImg, {
-      frameWidth: 7,
-      frameHeight: 6
-    });
+    this.load.image('about_tiles', aboutScreenSheet);
+    this.load.tilemapTiledJSON('about_tilemap', about);
   }
 
   create () {
     // adding tilemap
-    const map = this.make.tilemap({key: 'title_map'});
+    const map = this.make.tilemap({key: 'about_tilemap'});
   
     // Adding sprite sheet
-    const tileset = map.addTilesetImage('title_screen','title_tiles');
+    const tileset = map.addTilesetImage('about_tset','about_tiles');
 
     // add the background
     const background = map.createStaticLayer('background', tileset, 0,0);
@@ -58,11 +36,11 @@ export default class Title extends Phaser.Scene {
       button.name = buttonObject.name;
       button.setDisplaySize(buttonObject.width, buttonObject.height);
       button.enableBody = true;
-      button.setInteractive();
+      button.setInteractive({useHandCursor: true});
       button.alpha = 0.000000000000001;
       switch (button.name) {
-        case 'play':
-          button.on('pointerdown', () => this.scene.switch('TestDungeon'));
+        case 'back':
+          button.on('pointerdown', () => this.scene.switch('Title'));
           break;
         default:
           console.log('not implemented');
